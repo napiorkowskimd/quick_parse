d1e8c044d6635c73e65847347be3732f0fbc064b
Michal Napiorkowski <napiorkowskimd@gmail.com>
2022-11-27 18:07:11 +0100

    Fix CI builds #5

--- src/open_file.cpp
+++ src/open_file.cpp
@@ -1,13 +1,20 @@
 #include "open_file.hpp"
 
+#if _MSC_VER
+#pragma warning(disable:2220)
+#endif
+
+#if defined(__clang__) && _WIN32
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#endif
+
+
 gsl::owner<std::FILE *> OpenFile(const std::filesystem::path &path, const std::string &mode)
 {
-#if _WIN32
-  gsl::owner<std::FILE *> file{};
-  const auto err = std::fopen_s(&file, path.c_str(), mode.c_str());
-  if (err != 0) { return nullptr; }
-  return file;
-#else
   return std::fopen(path.c_str(), mode.c_str());
-#endif
 }
+
+#if defined(__clang__) && _WIN32
+#pragma clang diagnostic pop
+#endif
